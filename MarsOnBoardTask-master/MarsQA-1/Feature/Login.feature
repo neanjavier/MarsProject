8a8a97b538fd195af743f273293516c90e86a585
neanjavier <nean0723@gmail.com>
2021-12-06 12:23:05 +1100

    MarsProject
    Final version

---
+++ MarsOnBoardTask-master/MarsQA-1/Feature/Login.feature
@@ -0,0 +1,8 @@
+﻿Feature: Login
+	In order to get contacted
+	As a seller
+	I want the feature to add my profile details
+
+@mytag
+Scenario: Login to website
+	Given I login to the website
---
+++ MarsOnBoardTask-master/MarsQA-1/SpecflowPages/Helpers/ConstantHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsQA_1.Helpers
+{
+    public class ConstantHelpers
+    {
+        //Base Url
+        public static string Url = "http://localhost:5000";
+
+        //Excel path
+        public static string ExcelFile = @"C:\Users\Cloud\Documents\Nins\MVP\MarsOnBoardTask-master\MarsOnBoardTask-master\MarsQA-1\SpecflowTests\Data\Mars.xlsx";
+
+        //ScreenshotPath
+        public static string ScreenshotPath = "";
+
+        //ExtentReportsPath
+        public static string ReportsPath = "";
+
+        //ReportXML Path
+        public static string ReportXMLPath = "";
+    }
+}
